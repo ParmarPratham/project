b4cb1877528d9a2d9b82cce450c404a4622b137b
ParmarPratham <parmarpratham222@gmail.com>
2024-08-05 11:33:20 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar and Footer Example</title>
+    <link rel="stylesheet" href="css\style.css">
+</head>
+<body>
+    <footer>
+        <div class="footer-content">
+            <p>Copyright © 2014-2024 <a href="#">MINISTORE</a>. All rights reserved.</p>
+            <span class="version">Version 3.2.0</span>
+        </div>
+    </footer>
+</body>
+</html>
